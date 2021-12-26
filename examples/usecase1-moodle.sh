5510d5e4565a40fc2d6d0d03bd5e7958d7d6c0a0
terassyi <iscale821@gmail.com>
2021-12-26 15:21:04 +0900

    add example for moodle

---
+++ examples/usecase1-moodle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install docker
+sudo yum update -y
+sudo yum install -y docker
+# install git make
+sudo yum install -y git make wget
+
+# install docker-compose
+sudo wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/bin/docker-compose
+sudo chmod -v +x /usr/bin/docker-compose
+sudo chmod -v +x /usr/bin/docker-compose
+
+# enable docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+# get moodle docker-compose
+git clone https://github.com/bitnami/bitnami-docker-moodle.git
+
+# change settings for japanese
+sed -i -e "29i RUN echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen" bitnami-docker-moodle/3/debian-10/Dockerfile
+
+# sudo docker-compose up -d
